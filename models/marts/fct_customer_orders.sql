33f6bff02cff0237c5d2d3e2894f2389dc05720c
Mounica Kolukula <mounica168@gmail.com>
2022-10-13 14:42:43 +0000

    refactoring SQL

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -1,3 +1,83 @@
+with
+
+customers as (
+
+  select * from {{ ref('stg_jaffle_shop__customers') }}
+
+),
+
+
+paid_orders as (
+    select * from {{ {{ ref('inter_orders') }} }}
+/*
+  select 
+    orders.order_id,
+    orders.customer_id,
+    orders.order_placed_at,
+    orders.order_status,
+
+    completed_payments.total_amount_paid,
+    completed_payments.payment_finalized_date,
+
+    customers.customer_first_name,
+    customers.customer_last_name
+  from orders
+  left join completed_payments on orders.order_id = completed_payments.order_id
+  left join customers on orders.customer_id = customers.customer_id
+*/
+),
+
+-- Final CTE
+
+final as (
+
+  select
+    order_id,
+    customer_id,
+    order_placed_at,
+    order_status,
+    total_amount_paid,
+    payment_finalized_date,
+    customer_first_name,
+    customer_last_name,
+
+    -- sales transaction sequence
+    row_number() over (order by order_id) as transaction_seq,
+
+    -- customer sales sequence
+    row_number() over (partition by customer_id order by order_id) as customer_sales_seq,
+
+    -- new vs returning customer
+    case  
+      when (
+      rank() over (
+      partition by customer_id
+      order by order_placed_at, order_id
+      ) = 1
+    ) then 'new'
+    else 'return' end as nvsr,
+
+    -- customer lifetime value
+    sum(total_amount_paid) over (
+      partition by customer_id
+      order by order_placed_at
+      ) as customer_lifetime_value,
+
+    -- first day of sale
+    first_value(order_placed_at) over (
+      partition by customer_id
+      order by order_placed_at
+      ) as fdos
+
+    from paid_orders
+		
+)
+
+-- Simple Select Statement
+
+select * from final
+--order by order_id
+/*
 WITH paid_orders as (select Orders.ID as order_id,
         Orders.USER_ID    as customer_id,
         Orders.ORDER_DATE AS order_placed_at,
@@ -45,3 +125,4 @@ select
         order by p.order_id
     ) x on x.order_id = p.order_id
     ORDER BY order_id
+    */
